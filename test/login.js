955409d7e36fdf00bdad107701445bbc32626889
Sander Laarhoven <sander@laarhoven.org>
2017-01-11 19:23:17 +0100

    Improve login test.

--- test/login.js
+++ test/login.js
@@ -1,6 +1,10 @@
 const fritz = require('../index.js')
 const options = require('../package.json').options
+console.log(options)
 fritz.getSessionID(options)
 .then((sid) => {
-  console.log(sid)
+  console.log('SID:',sid)
+})
+.catch((error) => {
+  console.log('Error:',error)
 })
