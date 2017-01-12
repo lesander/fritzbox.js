bfd50f345224d741045749938f279ff1387013d6
Sander Laarhoven <sander@laarhoven.org>
2017-01-12 14:50:14 +0100

    Remove unnecessary escape characters

--- test/downloadmessage.js
+++ test/downloadmessage.js
@@ -1,7 +1,7 @@
 const fritz = require('../index.js')
 const options = require('../package.json').options
 console.log(options)
-const messagePath = '\/data\/tam\/rec\/rec.0.005'
+const messagePath = '/data/tam/rec/rec.0.005'
 fritz.downloadTamMessage(messagePath, options)
 .then((message) => {
   console.log('TAM message:', message.headers)
