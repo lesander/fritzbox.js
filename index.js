85b95eb2b3f454c9d8308a6132d9f70f40715e74
Sander Laarhoven <sander@laarhoven.org>
2017-01-15 20:39:57 +0100

    Tidy up index file.

--- index.js
+++ index.js
@@ -9,12 +9,16 @@ const fritzConfig = {
   version: require('./package.json').version,
   debug: require('./package.json').options.debug
 }
+
 const fritzRequest = require('./src/request.js')
 const fritzLogin = require('./src/login.js')
 const fritzFon = require('./src/fon.js')
 const fritzDect = require('./src/dect.js')
 const fritzWlan = require('./src/wlan.js')
 
-const fritz = Object.assign({}, fritzConfig, fritzRequest, fritzLogin, fritzFon, fritzDect, fritzWlan)
+const fritz = Object.assign(
+  {}, fritzConfig, fritzRequest, fritzLogin,
+  fritzFon, fritzDect, fritzWlan
+)
 
 module.exports = fritz
