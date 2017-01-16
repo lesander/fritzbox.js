9a807c960b3b3eb2553ba5711e59e1f640205b80
Sander Laarhoven <sander@laarhoven.org>
2017-01-16 21:33:35 +0100

    Return object instead of JSON string for getSmartDevices

--- test/smartdevices.js
+++ test/smartdevices.js
@@ -3,7 +3,7 @@ const options = require('../package.json').options
 console.log(options)
 fritz.getSmartDevices(options)
 .then((devices) => {
-  console.log('Devices:', devices)
+  console.log('Got ', devices.length, ' DECT Smart Home devices.')
 })
 .catch((error) => {
   console.log('Error:', error)
