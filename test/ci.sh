1f7f7ba83cb1e083ec5536a83967f6fe519c1a3d
Sander Laarhoven <sander@laarhoven.org>
2017-01-16 21:36:01 +0100

    Use test scripts locally.

--- test/ci.sh
+++ test/ci.sh
@@ -1,10 +1,16 @@
 #/usr/bin/env/sh
 set -e
+pwd
 
 # Check for runtime errors.
 echo "[*] Checking index.js for runtime errors."
 node index.js
 
-# No automated tests for testfiles exist yet.
+# Test some features.
+node test/login.js
+node test/calls.js
+node test/smartdevices.js
+node test/tam.js
+node test/phonebook.js
 
 echo "[*] Finished with tests"
