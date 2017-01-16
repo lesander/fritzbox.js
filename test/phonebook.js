5f2d1374d95edafe7df7cfd1fee7ce5c4d0eeb4f
Sander Laarhoven <sander@laarhoven.org>
2017-01-16 21:34:25 +0100

    Fix getPhonebook test.

--- test/phonebook.js
+++ test/phonebook.js
@@ -2,9 +2,9 @@ const fritz = require('../index.js')
 const options = require('../package.json').options
 const phonebookId = 0
 const localPath = '/Users/sander/Desktop/phonebook-0.json'
-fritz.getPhonebook(phonebookId, localPath, options)
-.then((message) => {
-  console.log(JSON.stringify(message, null, 2))
+fritz.getPhonebook(phonebookId, options)
+.then((phonebook) => {
+  console.log('Got ', phonebook.length, ' contacts.')
 })
 .catch((error) => {
   console.log('Error:', error)
