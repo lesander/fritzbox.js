21f7a907f8fa2794c4727b02cbc5d1b605901d1f
Sander Laarhoven <sander@laarhoven.org>
2017-01-16 21:49:01 +0100

    Standard --fix
    <3 standardjs.com

--- test/phonebook.js
+++ test/phonebook.js
@@ -1,7 +1,6 @@
 const fritz = require('../index.js')
 const options = require('../package.json').options
 const phonebookId = 0
-const localPath = '/Users/sander/Desktop/phonebook-0.json'
 fritz.getPhonebook(phonebookId, options)
 .then((phonebook) => {
   console.log('Got ', phonebook.length, ' contacts.')
