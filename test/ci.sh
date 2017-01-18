a6957d3ca37a30c2e2378ae084e0d6e21f5b57cb
Sander Laarhoven <sander@laarhoven.org>
2017-01-18 15:46:50 +0100

    Tweak test ci script.

--- test/ci.sh
+++ test/ci.sh
@@ -3,14 +3,16 @@ set -e
 pwd
 
 # Check for runtime errors.
-echo "[*] Checking index.js for runtime errors."
+echo '   Checking index.js for runtime errors..'
 node index.js
+echo ' ✓ No syntax errors found.'
 
 # Test some features.
+echo '   Running test scripts..'
 node test/login.js
 node test/calls.js
 node test/smartdevices.js
 node test/tam.js
 node test/phonebook.js
 
-echo "[*] Finished with tests"
+echo " ✓ Finished with tests."
