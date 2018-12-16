9afcd764dc027aeea6eca78adc41f6211582f3ae
Sander Laarhoven <sander@laarhoven.org>
2018-12-16 17:15:00 +0100

    Implement isWlanEncrypted and getWlanWPAType

--- src/wlan.js
+++ src/wlan.js
@@ -10,11 +10,12 @@ const jsdom = require('jsdom')
 const { JSDOM } = jsdom
 
 /**
- * Get the network key (PSK)
+ * Obtain a DOM object of the wireless security page.
  * @param  {Object} options - FritzBox.js options object.
- * @return {string} Returns the PSK.
+ * @return {Object} JSDOM instance
+ * @ignore
  */
-fritzWlan.getWlanKey = async (options) => {
+fritzWlan.getWirelessSecurityPage = async (options) => {
   // Obtain an SID first if none is yet set.
   if (!options.sid) {
     options.sid = await fritzLogin.getSessionId(options)
@@ -34,6 +35,39 @@ fritzWlan.getWlanKey = async (options) => {
   // Unfortunately, this section of Fritz!Box's interface is not modernized yet.
   // We're using JSDOM to parse the contents of the html result.
   const dom = new JSDOM(requestResult.body)
+  return dom
+}
+
+/**
+ * Check if the Fritz!Box's WLAN interface is password protected or not.
+ * @param  {Object} options - FritzBox.js options object.
+ * @return {boolean}
+ */
+fritzWlan.isWlanEncrypted = async (options) => {
+  const dom = await fritzWlan.getWirelessSecurityPage(options)
+  const selectedLevel = dom.window.document.querySelector('input[name=SecLevel][checked=checked]').value
+  return (selectedLevel === 'wpa')
+}
+
+/**
+ * Get the network key (PSK) of the Fritz!Box WLAN.
+ * @param  {Object} options - FritzBox.js options object.
+ * @return {string} Returns the PSK.
+ */
+fritzWlan.getWlanKey = async (options) => {
+  const dom = await fritzWlan.getWirelessSecurityPage(options)
   const key = dom.window.document.querySelector('input[name=pskvalue]').value
   return key
+}
+
+/**
+ * Get the type of WPA which is used by the Fritz!Box.
+ * @param  {Object} options - FritzBox.js options object.
+ * @return {string} wpa2|wpamixed
+ */
+fritzWlan.getWlanWPAType = async (options) => {
+  const dom = await fritzWlan.getWirelessSecurityPage(options)
+  const WPATypeSelect = dom.window.document.querySelector('select[name=wpa_type]')
+  const selectedWPAType = WPATypeSelect.querySelector('option[selected]').value
+  return selectedWPAType
 }
