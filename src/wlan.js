0b2389a87f85de16fff21b76f9d4b1c0d07e9550
Marvin <frankem@uni-bremen.de>
2021-04-25 17:59:44 +0200

    Add getWlanNetwork

--- src/wlan.js
+++ src/wlan.js
@@ -6,6 +6,17 @@ let fritzWlan = {}
 
 const fritzRequest = require('./request.js')
 
+fritzWlan.getWlanNetwork = async (options) => {
+  const form = {
+    page: 'wSet'
+  }
+
+  const response = await fritzRequest.request('/data.lua', 'POST', options, false, false, form)
+  if (response.error) return response
+
+  return JSON.parse(response.body).data.wlanSettings
+}
+
 fritzWlan.getWlanSecurity = async (options) => {
   const form = {
     page: 'wKey'
--- test/wlanSecurity.js
+++ test/wlanSecurity.js
@@ -2,16 +2,23 @@ const fritz = require('../index.js')
 const options = require('../package.json').options
 
 async function wlanSecurity () {
-  const info = await fritz.getWlanSecurity(options)
+  const network = await fritz.getWlanNetwork(options)
+  const security = await fritz.getWlanSecurity(options)
 
-  if (info.error) {
-    console.log('Error:', info.error.message)
+  if (network.error) {
+    console.log('Error:', network.error.message)
+    process.exit(1)
+  }
+
+  if (security.error) {
+    console.log('Error:', security.error.message)
     process.exit(1)
   }
 
   console.log(`WLAN Info:\n` +
-    `\tSSID: ${info.ssid}\n` +
-    `\tPSK: ${info.psk}`)
+    `\tSSID: ${security.ssid}\n` +
+    `\tPSK: ${security.psk}\n` +
+    `\tHidden ${network.hiddenSSID}`)
 }
 
 wlanSecurity()
