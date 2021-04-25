242faa091cd1e8be76c32cf13188b04d5cb37977
Marvin <frankem@uni-bremen.de>
2021-04-25 15:59:23 +0200

    change variable name

--- test/wlanSecurity.js
+++ test/wlanSecurity.js
@@ -2,16 +2,16 @@ const fritz = require('../index.js')
 const options = require('../package.json').options
 
 async function wlanSecurity () {
-  const calls = await fritz.getWlanSecurity(options)
+  const info = await fritz.getWlanSecurity(options)
 
-  if (calls.error) {
-    console.log('Error:', calls.error.message)
+  if (info.error) {
+    console.log('Error:', info.error.message)
     process.exit(1)
   }
 
   console.log(`WLAN Info:\n` +
-    `\tSSID: ${calls.ssid}\n` +
-    `\tPSK: ${calls.psk}`)
+    `\tSSID: ${info.ssid}\n` +
+    `\tPSK: ${info.psk}`)
 }
 
 wlanSecurity()
